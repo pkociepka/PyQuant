3701896bafae317820358742062e9ba30ddfe6a3
Piotr Ociepka <pkociepka@gmail.com>
2016-01-13 17:44:05 +0100

    Controlled gates.

--- gates.py
+++ gates.py
@@ -1,10 +1,10 @@
-from numpy import exp, pi, sqrt
+from numpy import exp, pi, sqrt, add
 from q_math import *
 
 I = [[1, 0],
      [0, 1]]
 
-Id = lambda n: __kron([I]*n)
+Id = lambda n: kron([I]*n)
 
 X = [[0, 1],
      [1, 0]]
@@ -21,7 +21,18 @@ H = dot([1/sqrt(2), [[1, 1],
 R = lambda phi: [[1, 0],
                  [0, exp(1j*pi/phi)]]
 
-print(R(1))
-print(R(2))
-print(R(3))
-print(R(4))
+def controlled(gate, size=1, c_bit="up"):
+    Up_l = [[1,0],
+            [0,0]]
+    Lo_r = [[0,0],
+            [0,1]]
+
+    if c_bit == "up":
+        return add(kron([Up_l, Id(size)]), kron([Lo_r, gate]))
+    elif c_bit == "down":
+        return add(kron([Id(size), Up_l]), kron([gate, Lo_r]))
+
+Not = X
+CNot = controlled(X)
+CCNot = controlled(CNot, size=2)
+Toffoli = CCNot
