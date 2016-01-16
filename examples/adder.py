7b9f1a60c1613730cdf3cd24ae280f1a9b71e127
Piotr Ociepka <pkociepka@gmail.com>
2016-01-16 18:00:22 +0100

    Methods returning copies of cirquits.

--- examples/adder.py
+++ examples/adder.py
@@ -1,4 +1,4 @@
-from copy import copy as copy
+from copy import deepcopy as deepcopy
 import os, sys
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
@@ -13,6 +13,8 @@ import gates
 Sum = Cirquit(3).add_controlled_gate(gates.Not, 2, 1)\
                 .add_controlled_gate(gates.Not, 2, 0)
 
+sum = lambda: deepcopy(Sum)
+
 # carry gate
 # order of qubits in input register:
 #   | lower_, a, b, 0 >
@@ -24,6 +26,8 @@ Carry = Cirquit(4).add_double_controlled_gate(gates.Not, 3, [1, 2])\
                   .add_controlled_gate(gates.Not, 2, 1)\
                   .add_double_controlled_gate(gates.Not, 3, [0, 2])
 
+carry = lambda: deepcopy(Carry)
+
 # 1-qubit full adder
 # order of qubits in input register:
 #   | 0, a, b, 0 >
@@ -33,4 +37,4 @@ Carry = Cirquit(4).add_double_controlled_gate(gates.Not, 3, [1, 2])\
 #   for upper_carry qubit
 Adder = Cirquit(4).add_cirquit(Carry)\
                   .add_controlled_gate(gates.Not, 2, 1)\
-                  .add_cirquit(copy(Sum).extend(1, location="bottom"))
+                  .add_cirquit(sum().extend(1, location="bottom"))
